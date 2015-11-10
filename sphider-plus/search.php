209bc896de6ef7fa6fb30f69465e90aa1823305e
h0wn0w <howard.buddin@gmail.com>
2015-11-09 21:30:49 -0500

    settings for sp configured

--- sphider-plus/search.php
+++ sphider-plus/search.php
@@ -1,145 +1,84 @@
 <?php
-/*******************************************
-* Sphider Version 1.3.x
-* This program is licensed under the GNU GPL.
-* By Ando Saabas          ando(a t)cs.ioc.ee
-********************************************/
-//error_reporting(E_ALL ^ E_NOTICE ^ E_WARNING); 
-error_reporting(E_ALL); 
-$include_dir = "./include"; 
-include ("$include_dir/commonfuncs.php");
-//extract(getHttpVars());
+/*****************    Start of Sphider-plus scripts     ********************
+ *
+ *   Sphider-plus version 3.20153e created 2015-09-24
+ *
+ *   Based on original Sphider version 1.3.5
+ *   released: 2009-12-13
+ *   by Ando Saabas     http://www.sphider.eu
+ *
+ *   This program is licensed under the GNU GPL by:
+ *    Rolf Kellner  [Tec]   tec@sphider-plus.eu
+ *   Original Sphider GNU GPL licence by:
+ *   Ando Saabas   ando(a t)cs.ioc.ee
+ *
+*
+ *******************************************************************
+ */
+    //  for command line operation, correct the working directory
+    $dir0 = str_replace('\\', '/', __DIR__);
+    chdir($dir0);
 
-if (isset($_GET['query']))
-	$query = $_GET['query'];
-if (isset($_GET['search']))
-	$search = $_GET['search'];
-if (isset($_GET['domain'])) 
-	$domain = $_GET['domain'];
-if (isset($_GET['type'])) 
-	$type = $_GET['type'];
-if (isset($_GET['catid'])) 
-	$catid = $_GET['catid'];
-if (isset($_GET['category'])) 
-	$category = $_GET['category'];
-if (isset($_GET['results'])) 
-	$results = $_GET['results'];
-if (isset($_GET['start'])) 
-	$start = $_GET['start'];
-if (isset($_GET['adv'])) 
-	$adv = $_GET['adv'];
-	
-	
-$include_dir = "./include"; 
-$template_dir = "./templates"; 
-$settings_dir = "./settings"; 
-$language_dir = "./languages";
-
-
-require_once("$settings_dir/database.php");
-require_once("$language_dir/en-language.php");
-require_once("$include_dir/searchfuncs.php");
-require_once("$include_dir/categoryfuncs.php");
-
-
-include "$settings_dir/conf.php";
-
-include "$template_dir/$template/header.html";
-include "$language_dir/$language-language.php";
-
-
-if ($type != "or" && $type != "and" && $type != "phrase") { 
-	$type = "and";
-}
-
-if (preg_match("/[^a-z0-9-.]+/", $domain)) {
-	$domain="";
-}
-
-
-if ($results != "") {
-	$results_per_page = $results;
-}
-
-if (get_magic_quotes_gpc()==1) {
-	$query = stripslashes($query);
-} 
-
-if (!is_numeric($catid)) {
-	$catid = "";
-}
-
-if (!is_numeric($category)) {
-	$category = "";
-} 
-
-
-
-if ($catid && is_numeric($catid)) {
-
-	$tpl_['category'] = sql_fetch_all('SELECT category FROM '.$mysql_table_prefix.'categories WHERE category_id='.(int)$_REQUEST['catid']);
-}
-	
-$count_level0 = sql_fetch_all('SELECT count(*) FROM '.$mysql_table_prefix.'categories WHERE parent_num=0');
-$has_categories = 0;
-
-if ($count_level0) {
-	$has_categories = $count_level0[0][0];
-}
-
-
-
-require_once("$template_dir/$template/search_form.html");
-
-
-function getmicrotime(){
-    list($usec, $sec) = explode(" ",microtime());
-    return ((float)$usec + (float)$sec);
-    }
-
-
-
-function poweredby () {
-	global $sph_messages;
-    //If you want to remove this, please donate to the project at http://www.sphider.eu/donate.php
-    print $sph_messages['Powered by'];?>  <a href="http://www.sphider.eu/"><img src="sphider-logo.png" border="0" style="vertical-align: middle" alt="Sphider"></a>
-
-    <?php 
-}
-
-
-function saveToLog ($query, $elapsed, $results) {
-        global $mysql_table_prefix;
-    if ($results =="") {
-        $results = 0;
-    }
-    $query =  "insert into ".$mysql_table_prefix."query_log (query, time, elapsed, results) values ('$query', now(), '$elapsed', '$results')";
-	mysql_query($query);
-                    
-	echo mysql_error();
-                        
-}
-
-switch ($search) {
-	case 1:
-
-		if (!isset($results)) {
-			$results = "";
-		}
-		$search_results = get_search_results($query, $start, $category, $type, $results, $domain);
-		require("$template_dir/$template/search_results.html");
-	break;
-	default:
-		if ($show_categories) {
-			if ($_REQUEST['catid']  && is_numeric($catid)) {
-				$cat_info = get_category_info($catid);
-			} else {
-				$cat_info = get_categories_view();
-			}
-			require("$template_dir/$template/categories.html");
-		}
-	break;
-	}
-
-include "$template_dir/$template/footer.html";
+    // define secure constant
+    define("_SECURE",1);    // define secure constant
+/*
+/********************************************************************
+ *
+ *       The following 'include' contains some start-up variables.
+ *
+ *       Usually these variables are not needed to be modified.
+ *       Modifications are only required for applications,
+ *       which should overwrite the Admin settings
+ */
+    include_once "search_ini.php";
+/*
+ *******************************************************************
+ *
+ *       The following 'include' contains the HTML header.
+ *
+ *       For embedded application, only the Sphider-plus significant
+ *       part of the HTML header will be added. So, in this case,
+ *       be aware to place this 'include' inside of your HTML header.
+ *       For embedded application it is mandatory to active the according
+ *       setting in Admin backend.
+ *
+ */
+    include_once "$include_dir/search_10.php";
+/*
+ *******************************************************************
+ *
+ *       The following 'include' contains the 'NoJavaScript' warning message,
+ *       as well as the headline presented on top of the Search form.
+ */
+    include_once "$include_dir/search_20.php";
+/*
+ *******************************************************************
+ *
+ *       The following 'include' will add the 'Search form'
+ *       and, if requested, also the advanced options, as well as categories, etc.
+ */
+    include_once "$include_dir/search_30.php";
+/*
+ *******************************************************************
+ *
+ *       The following 'include' will add the result listings
+ *       for text and media search, link search and all other search modes.
+ */
+    include_once "$include_dir/search_40.php";
+/*
+ *******************************************************************
+ *
+ *       The following 'include' contains the
+ *       - Form for 'Suggest a new URL'
+ *       - Link to Sphider-plus project page
+ *       - HTML end tags
+ *
+ *       For embedded application, only the Sphider-plus significant
+ *       part of the footer will be added. So, in this case,
+ *       be aware to place this 'include' above your
+ *       </body> and </html> tags.
+ */
+    include_once "$include_dir/search_50.php";
+/*
+ *****************    End of Sphider-plus scripts     ********************/
 ?>
