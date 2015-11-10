83b25eeda7a937b633812fd6c9adac5ec155eff3
h0wn0w <howard.buddin@gmail.com>
2015-11-10 03:12:13 +0000

    added sphider-plus; renamed sphider to old

---
+++ sphider~old/settings/database.php
@@ -0,0 +1,19 @@
+<?php
+	$database="npnlook";
+	$mysql_user = "npnow";
+	$mysql_password = "park47&trunk";
+	$mysql_host = "serchin.db";
+	$mysql_table_prefix = "thx";
+
+
+
+	$success = mysql_pconnect ($mysql_host, $mysql_user, $mysql_password);
+	if (!$success)
+		die ("<b>Cannot connect to database, check if username, password and host are correct.</b>");
+    $success = mysql_select_db ($database);
+	if (!$success) {
+		print "<b>Cannot choose database, check if database name is correct.";
+		die();
+	}
+?>
+
