3f148df86aceb45ee78e4459717dd90deeb46cf8
Howard Buddin <howard.buddin@gmail.com>
2016-01-12 00:15:42 -0500

    Update database.php

--- sphider-pdo/settings/database.php
+++ sphider-pdo/settings/database.php
@@ -1,5 +1,5 @@
 <?php
-	define('TABLE_PREFIX', "dang");		/* typically only needed for MySQL */
-	define('DATABASE_NAME', 'sphider');	/* only needed for MySQL */
-  $db = new PDO('mysql:host=localhost; dbname='.DATABASE_NAME, 'root', 'h0wn0w');	
+	define('TABLE_PREFIX', "PREFIX");		/* typically only needed for MySQL */
+	define('DATABASE_NAME', 'DBNAME');	/* only needed for MySQL */
+  $db = new PDO('mysql:host=localhost; dbname='.DATABASE_NAME, 'USER', 'PW');	
 ?>
